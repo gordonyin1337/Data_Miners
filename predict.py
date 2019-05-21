50d870ac21ced3a0e70f7bca840414cdd91f7a81
Daniel Murphy <daniel.murphyx@gmail.com>
2019-05-21 10:33:09 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,9 +5,9 @@ from skimage import transform
 import os
 
 
-def classify(model_name,input_folder):
+def predict(model_name,input_folder):
     model = load_model(model_name)
-    my_dict = {0: 'ColdTaiga', 1: 'IceMountains', 2: 'IcePlains'}
+    name_dict = {0: 'ColdTaiga', 1: 'IceMountains', 2: 'IcePlains'}
     predictions = []
     for filename in os.listdir(input_folder):
         image = Image.open(input_folder + '/' + filename)
@@ -18,13 +18,13 @@ def classify(model_name,input_folder):
         predictions.append(result)
 
     max_occurrence = max(predictions, key=predictions.count)
-    return my_dict[max_occurrence]
+    return name_dict[max_occurrence]
 
 
 def main():
     model_name = 'conv_network.h5' #this should stay the same
     input_folder = 'C:/test' #change this to your own location
-    biome_prediction = classify(model_name,input_folder)
+    biome_prediction = predict(model_name,input_folder)
     print(biome_prediction)
 
 
