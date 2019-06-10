05e0715f7f8498f913c241de51523afb3e842a50
gordonyin1337 <gordon.yin1337@gmail.com>
2019-06-10 14:44:51 -0700

    Update build_biomes.py

--- src/build_biomes.py
+++ src/build_biomes.py
@@ -11,8 +11,6 @@ import random
 MINECRAFT_WORLDS = "C:\\Malmo2\\Minecraft\\run\\saves\\"
 RECORDINGS = "C:\\Malmo2\\CS175_Homework\\Data_Miners\\Recordings"
 
-biome = "IcePlains"
-
 if sys.version_info[0] == 2:
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 else:
@@ -20,44 +18,46 @@ else:
 
     print = functools.partial(print, flush=True)
 
-missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+def create_mission(biome):
+    x_value = '''"''' + str(random.randint(1, 50000)) + '''"'''
+    y_value = '''"90"'''
+    z_value = '''"''' + str(random.randint(1, 50000)) + '''"'''
 
-              <About>
-                <Summary>Hello world!</Summary>
-              </About>
+    missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+                <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
-              <ServerSection>
-                <ServerInitialConditions>
-                  <Time>
-                      <StartTime>1000</StartTime>
-                      <AllowPassageOfTime>false</AllowPassageOfTime>
-                  </Time>
-                  <Weather>clear</Weather>
-                </ServerInitialConditions>
-                <ServerHandlers>
-                  <FileWorldGenerator src="C:\\Malmo2\\Minecraft\\run\\saves\\''' + biome + '''"/>
-                  <ServerQuitWhenAnyAgentFinishes/>
-                </ServerHandlers>
-              </ServerSection>
+                  <About>
+                    <Summary>Biome Classification</Summary>
+                  </About>
 
-              <AgentSection mode="Creative">
-                <Name>MalmoTutorialBot</Name>
-                <AgentStart>
-                </AgentStart>
-                <AgentHandlers>
-                  <MissionQuitCommands/>
-                  <AbsoluteMovementCommands/>
-                  <ObservationFromGrid>
-                    <Grid name="observation">
-                        <min x="-25" y="-25" z="-25"/>
-                        <max x="24" y="24" z="24"/>
-                    </Grid>
-                  </ObservationFromGrid>
-                  <ContinuousMovementCommands turnSpeedDegs="180"/>
-                </AgentHandlers>
-              </AgentSection>
-            </Mission>'''
+                  <ServerSection>
+                    <ServerInitialConditions>
+                      <Time>
+                          <StartTime>1000</StartTime>
+                          <AllowPassageOfTime>false</AllowPassageOfTime>
+                      </Time>
+                      <Weather>clear</Weather>
+                    </ServerInitialConditions>
+                    <ServerHandlers>
+                      <FileWorldGenerator src="''' + MINECRAFT_WORLDS + biome + '''"/>
+                    </ServerHandlers>
+                  </ServerSection>
+
+                  <AgentSection mode="Creative">
+                    <Name>Data_Miner</Name>
+                    <AgentStart>
+                        <Placement x='''+x_value+''' y='''+y_value+''' z='''+z_value+'''/> 
+                    </AgentStart>
+                    <AgentHandlers>
+                      <ChatCommands/>
+                      <MissionQuitCommands/>
+                      <AbsoluteMovementCommands/>
+                      <ObservationFromSystem/>
+                      <ContinuousMovementCommands turnSpeedDegs="180"/>
+                    </AgentHandlers>
+                  </AgentSection>
+                </Mission>'''
+    return missionXML
 
 agent_host = MalmoPython.AgentHost()
 try:
@@ -70,71 +70,74 @@ if agent_host.receivedArgument("help"):
     print(agent_host.getUsage())
     exit(0)
 
-my_mission = MalmoPython.MissionSpec(missionXML, True)
-my_mission_record = MalmoPython.MissionRecordSpec()
 
-# Attempt to start a mission:
-max_retries = 3
-for retry in range(max_retries):
-    try:
-        agent_host.startMission( my_mission, my_mission_record )
-        break
-    except RuntimeError as e:
-        if retry == max_retries - 1:
-            print("Error starting mission:",e)
-            exit(1)
-        else:
-            time.sleep(2)
+def get_biome_data(biome):
+    # Attempt to start a mission:
+    my_mission = MalmoPython.MissionSpec(create_mission(biome), True)
+    my_mission_record = MalmoPython.MissionRecordSpec()
 
-# Loop until mission starts:
-print("Waiting for the mission to start ", end=' ')
-world_state = agent_host.getWorldState()
-while not world_state.has_mission_begun:
-    print(".", end="")
-    time.sleep(0.1)
+    max_retries = 3
+    for retry in range(max_retries):
+        try:
+            agent_host.startMission(my_mission, my_mission_record)
+            break
+        except RuntimeError as e:
+            if retry == max_retries - 1:
+                print("Error starting mission:",e)
+                exit(1)
+            else:
+                time.sleep(2)
+
+    # Loop until mission starts:
+    print("Waiting for the mission to start ", end=' ')
     world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print("Error:",error.text)
+    while not world_state.has_mission_begun:
+        print(".", end="")
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        for error in world_state.errors:
+            print("Error:",error.text)
 
-print()
-print("Mission running ", end=' ')
+    print()
+    print("Mission running ", end=' ')
 
-counting_dict = None
+    counter = 0
 
-counter = 0
-
-for x in range(10):
-    for z in range(10):
-        world_state = agent_host.peekWorldState()
-        teleport_x = random.randint(1,80000)
-        teleport_z = random.randint(1,80000)
-        tp_command = "tp " + str(teleport_x)+ " 100 " + str(teleport_z)
-        print("Sending command: " + tp_command)
-        agent_host.sendCommand(tp_command)
-        time.sleep(4)
-        agent_host.sendCommand("setPitch 0")
-        takess.simg("Screenshots//" + biome + "_" + str(counter))
-        counter += 1
-        for i in range(7):
-            agent_host.sendCommand("turn 0.5")
-            time.sleep(0.5)
-            agent_host.sendCommand("turn 0")
-            takess.simg("Screenshots//" + biome + "_" + str(counter))
+    if not os.path.exists("Screenshots//" + biome):
+        os.mkdir("Screenshots//" + biome)
+    for x in range(10):
+        for z in range(10):
+            world_state = agent_host.peekWorldState()
+            teleport_x = random.randint(1,80000)
+            teleport_z = random.randint(1,80000)
+            tp_command = "tp " + str(teleport_x)+ " 90 " + str(teleport_z)
+            print("Sending command: " + tp_command)
+            agent_host.sendCommand(tp_command)
+            time.sleep(4)
+            agent_host.sendCommand("setPitch 0")
+            takess.simg("Screenshots//" + biome + "//" + biome + "_" + str(counter))
             counter += 1
-            time.sleep(0.5)
+            for i in range(7):
+                agent_host.sendCommand("turn 0.5")
+                time.sleep(0.5)
+                agent_host.sendCommand("turn 0")
+                takess.simg("Screenshots//" + biome + "//" + biome + "_" + str(counter))
+                counter += 1
+                time.sleep(0.5)
 
+    # Loop until mission ends:
+    agent_host.sendCommand("quit")
+    while world_state.is_mission_running:
+        print(".", end="")
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        for error in world_state.errors:
+            print("Error:",error.text)
 
+    print()
+    print("Mission ended")
+    # Mission has ended.
 
-# Loop until mission ends:
-agent_host.sendCommand("quit")
-while world_state.is_mission_running:
-    print(".", end="")
-    time.sleep(0.1)
-    world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print("Error:",error.text)
-
-print()
-print("Mission ended")
-# Mission has ended.
-
+missions_biomes = ["MegaTaiga", "ForestHills", "TaigaHills", "JungleHills", "BirchForestHills", "MegaTaigaHills"]
+for b in missions_biomes:
+    get_biome_data(b)
